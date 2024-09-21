c89e17c4d8ea04b19d36d68af9fc8c50aa28ee3a
isinha <00001106898825sp@al.educacao.sp.gov.br>
2024-09-20 21:05:21 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+let cor;
+let circuloX;
+let circuloY;
+
+function setup() {
+  createCanvas(400, 400);
+  background(color(100,0,0));
+  cor = color(random(0,255), random(0,255));
+  circuloX = [0,0,0];
+  circuloY = [random(height), random(height),random(height)];
+}
+  
